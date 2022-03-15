07056093d856a5cb08623608f7568ce09cab554c
Sanskar <sanskaramgain@gmail.com>
2022-03-15 20:40:29 +0545

    Deep learning architecture added with working backend server (#4)
    * Latent Image translation Added and integrated with backend
    
    * Code refactored to some extent
    
    * pix2pix model code added
    
    * Useless file removed and SSIM score testing added
    
    * Repo refactored
    
    * Code refactored
    
    * Update README.md

--- webpage/views.py
+++ webpage/views.py
@@ -4,7 +4,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .bringing_old_photos_back_to_life import model
 import os
-import shutil
 import torch
 from rest_framework.decorators import api_view
 from django.shortcuts import render
@@ -12,16 +11,14 @@ from .models import Image
 from rest_framework import generics 
 from .serializers import ImageSerializer 
 from rest_framework.parsers import MultiPartParser, FormParser
-from .conver_image import convert    
+from .convert_image import convert    
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .conver_image import convert
 
 
 # Create your views here.
 # TODO: Remove this
 def home(request):
-	# work()
 	torch.cuda.empty_cache()
 	current_working_directory = os.getcwd().split('/')[-1]
 	path = "webpage/pix2pix_super_res/outputs/input.jpg"
@@ -43,13 +40,13 @@ class ImageList(APIView):
         return Response(serializer.data)
 
     def post(self, request,*args, **kwargs):
-        for x in Image.objects.all():
-            x.delete()
-        print(request.data)
+        # delete all the images from the database
+        for image in Image.objects.all():
+            image.delete()
         images_serializer = ImageSerializer(data=request.data)
         if images_serializer.is_valid():
             images_serializer.save()
-            convert()
+            convert(images_serializer['method'].value)
             return Response(images_serializer.data,)
         else:
             print(f"error, {images_serializer.errors}")
